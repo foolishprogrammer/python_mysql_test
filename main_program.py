37df627bebd38f76d46d3240813b55e1b289def7
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2022-07-08 10:19:39 +0700

    removing __pycache__ from the git

--- main_program.py
+++ main_program.py
@@ -7,5 +7,13 @@ users_query = "SELECT * FROM users"
 if(query.execute(users_query)) !=0 :
     users_list = query.fetchall()
 
-for i in users_list:
+data_query = "SELECT * FROM data"
+
+if (query.execute(data_query)) !=0 :
+    data_list = query.fetchall()
+
+for i in users_list :
+    print(i)
+
+for i in data_list :
     print(i)
