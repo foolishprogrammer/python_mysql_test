1e546893a908a56218e568e35cbc1027131589d2
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2022-07-21 00:51:14 +0700

    Fixing the type hint and reduce the code

--- main_program.py
+++ main_program.py
@@ -14,7 +14,7 @@ class dataDatabase:
     response: list[str] = []
     confirmed: list[str] = []
 
-    def __init__(self, data_query) -> None:
+    def __init__(self, data_query: list) -> None:
         for i in data_query:
             self.response_id.append(i[0])
             self.user_id.append(i[1])
@@ -27,30 +27,30 @@ class dataDatabase:
         return key[1]
 
     # rank the user based on the top user
-    def top_rank(self, rank: int, choice: str) -> list[tuple[int]]:
+    def top_rank(self, rank: int, choice: str) -> list[tuple]:
         data_uid = list(set(self.user_id))
 
         uid_confirmed = [
             (self.user_id[i], self.confirmed[i]) for i in range(len(self.user_id))
         ]
         # function to call for each choice
-        def confirmed_yes(data=uid_confirmed) -> list[tuple[int]]:
+        def confirmed_yes(data=uid_confirmed) -> list[tuple]:
             value_count = []
 
             for i in data_uid:
-                value_count.append((i, data.count(i, "yes")))
+                value_count.append((i, data.count((i, "yes"))))
 
             return value_count
 
-        def confirmed_no(data=uid_confirmed) -> list[tuple[int]]:
+        def confirmed_no(data=uid_confirmed) -> list[tuple]:
             value_count = []
 
             for i in data_uid:
-                value_count.append((i, data.count(i, "no")))
+                value_count.append((i, data.count((i, "no"))))
 
             return value_count
 
-        def confirmed_all(data=uid_confirmed) -> list[tuple[int]]:
+        def confirmed_all(data=uid_confirmed) -> list[tuple]:
             value_count = []
 
             for i in data_uid:
@@ -64,7 +64,31 @@ class dataDatabase:
         OPTION = {"yes": confirmed_yes, "no": confirmed_no}
         # use confirmed_all as the default value
         ranked = OPTION.get(choice, confirmed_all)()
-        ranked.sort(self.sort_key, reverse=True)
+        ranked.sort(key=self.sort_key, reverse=True)
+        return ranked[:rank]
+
+    # counting the difference between prompt and response
+    # and count how many times the each user id that fit the condition given
+    # (in this case, if the difference is bigger than given value)
+    def top_difference(
+        self, condition: int, confirmation: str, rank: int = 1
+    ) -> list[tuple]:
+        ranked: list = []
+        difference_count: list = []
+
+        def prompt_response_difference(prompt: str, response: str) -> int:
+            return len(response.split()) - len(prompt.split())
+
+        for i in range(len(self.user_id)):
+            difference_count.append(
+                (
+                    self.user_id[i],
+                    prompt_response_difference(self.prompt[i], self.response[i]),
+                )
+                if (self.confirmed[i] == confirmation or confirmation == "all")
+                else None
+            )
+
         return ranked[:rank]
 
 
@@ -77,6 +101,14 @@ class userDatabase:
         for i in user_query:
             self.uid.append(i[0])
             self.uname.append(i[1])
+
+    def retrieve_name(self, check_uid: list) -> list[str]:
+        name_list: list = []
+
+        for i in check_uid:
+            name_list.append(self.uname[self.uid.index(i[0])])
+
+        return name_list
 
 
 # Running the program
@@ -98,6 +130,8 @@ def main() -> None:
     data_db = query.fetchall()
 
     data = dataDatabase(data_db)
+
+    print(user.retrieve_name(data.top_rank(3, "yes")))
 
 
 if __name__ == "__main__":
